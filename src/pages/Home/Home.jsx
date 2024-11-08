927394b7713c963196035dd415b0ff23116b9e7f
Aviral <aviral.vbansal16@gmail.com>
2024-11-08 23:56:57 +0530

    Setting up the notes page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
 
 const Home = () => {
   return (
     <>
       <Navbar/>
+
+      <div className='container mx-auto'>
+
+
+        <NoteCard 
+        title="Meeting" 
+        date="3rd May" 
+        content="Cool"
+        tags="#Meeting"
+        isPinned={true}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+        />
+        
+
+
+      </div>
     
     
     

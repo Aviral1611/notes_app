3575a9cd89830ead48e6e457521accb6c135513c
Aviral <aviral.vbansal16@gmail.com>
2024-11-17 01:20:32 +0530

    Set up the basic frontend

--- src/pages/Home/AddEditNotes.jsx
+++ src/pages/Home/AddEditNotes.jsx
@@ -1,14 +1,56 @@
 import React, { useState } from 'react'
 import TagInput from '../../components/Input/TagInput';
+import { MdClose } from 'react-icons/md';
 
-const AddEditNotes = () => {
+const AddEditNotes = ({noteData, type, onClose}) => {
 
   const [title,setTitle] = useState("")
   const [content, setContent] = useState("")
   const [tags, setTags] = useState([]);
 
+  const [error, setError] = useState(null);
+
+
+  const addNewNote = async () => {};
+
+  const editNote = async () => {};
+
+
+  const handleAddNote = () =>{
+    if (!title) {
+      setError("Please Enter a title")
+      return;
+    }
+
+    if (!content) {
+      setError("Please Enter a content")
+      return;
+    }
+
+    setError("");
+
+    if (type=='edit'){
+      editNote()
+    }else{
+      addNewNote()
+    
+    }
+  }
+
   return (
     <div>
+
+      <div className='relative'> 
+        <button
+        className='w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50'
+        onClick={onClose}>
+        <MdClose className='text-xl text-slate-400' />
+        </button>
+      </div>
+
+
+
+
       <div className='flex flex-col gap-3'>
         <label className='input-label'>TITLE</label>
         <input
@@ -34,10 +76,14 @@ const AddEditNotes = () => {
 
       <div className='mt-3'>
         <label className='input-label'>TAGS</label>
-        <TagInput />
+        <TagInput tags={tags} setTags={setTags} />
       </div>
 
-      <button className='btn-primary font-medium mt-5 pt-3' onClick={() => {}}>
+
+      {error && <p className='text-red-600 text-xs pt-3'>{error}</p>}
+
+      <button className='btn-primary font-medium mt-5 pt-3' 
+      onClick={handleAddNote}>
         ADD
       </button>
     </div>
